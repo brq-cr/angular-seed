751986d7f98c845a935d9069f6d0223103d210b0
BRYAN RAMIREZ QUIROS <b.rq@me.com>
2017-08-17 21:08:34 -0600

    - upgrating dependencies - removing apollo client

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AngularBasePage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('angular-base App', function() {
-  let page: AngularBasePage;
+describe('cli-update App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AngularBasePage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
